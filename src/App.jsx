cbce3d1ada9540cc62065323d2a07c774392a8fa
hossam43 <hossamayman876.gmail.com>
2023-07-23 00:46:50 +0300

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
   const [cardData, setCardData] = useState([]);
   const [filter, setFilter] = useState([]);
   const [activeCategory, setActiveCategory] = useState("all");
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const data = await fetch("https://data-project-19is.onrender.com/design");
     const cards = await data.json();
-    console.log(cards);
+
     setCardData(cards);
     setFilter(cards);
+    setLoading(false);
   };
   return (
     <div className="App">
@@ -25,14 +27,17 @@ function App() {
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
       ></Filter>
-
-      <motion.div layout className="cards-container">
-        <AnimatePresence>
-          {filter.map((card) => {
-            return <Cards key={card.id} card={card}></Cards>;
-          })}
-        </AnimatePresence>
-      </motion.div>
+      {loading ? (
+        <h1 className="spinner">Loading...</h1>
+      ) : (
+        <motion.div layout className="cards-container">
+          <AnimatePresence>
+            {filter.map((card) => {
+              return <Cards key={card.id} card={card}></Cards>;
+            })}
+          </AnimatePresence>
+        </motion.div>
+      )}
     </div>
   );
 }
